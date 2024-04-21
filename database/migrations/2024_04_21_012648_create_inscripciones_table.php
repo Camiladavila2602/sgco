2bc468d1ebee0b72bb2b91fd733eb42181a98c20
Dayana Cordoba <dcordoba3341@campuscecep.edu.co>
2024-04-20 20:32:49 -0500

    Creacion de migraciones y tablas en la bd

---
+++ database/migrations/2024_04_21_012648_create_inscripciones_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inscripciones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('curso_id');
+            $table->integer('instructor_id');
+            $table->integer('estudiante_id');
+            $table->date('fecha_inscripcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscripciones');
+    }
+};
