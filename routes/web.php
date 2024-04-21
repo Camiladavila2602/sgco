94addb5d143f3fa1f82e5012c43bc4db12a81c1b
Dayana Cordoba <camiladavila2602@gmail.com>
2024-04-21 14:47:14 -0500

    Creacion Autenticacion

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,29 @@
 <?php
-
 use App\Http\Controllers\CursoController;
 use App\Http\Controllers\EstudianteController;
 use App\Http\Controllers\InscripcionController;
 use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::middleware('auth')->group(function () {
 
 Route::get('/curso', [CursoController:: class, 'index'])->name('cursos.index');
 Route::post('/curso', [CursoController::class, 'store'])->name('cursos.store');
@@ -16,14 +31,12 @@ Route::get('/curso/create', [CursoController::class, 'create'])->name('cursos.cr
 Route::delete('/curso/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
 Route::put('/curso/{curso}', [CursoController::class, 'update'])->name('cursos.update');
 Route::get('/curso/{curso}/edit', [CursoController::class , 'edit'])->name('cursos.edit');
-
 Route::get('/instructores', [InstructorController:: class, 'index'])->name('instructor.index');
 Route::post('/instructores', [instructorController::class, 'store'])->name('instructor.store');
 Route::get('/instructores/create', [instructorController::class, 'create'])->name('instructor.create');
 Route::delete('/instructores/{instructor}', [instructorController::class, 'destroy'])->name('instructor.destroy');
 Route::put('/instructores/{instructor}', [instructorController::class, 'update'])->name('instructor.update');
 Route::get('/instructores/{instructor}/edit', [instructorController::class , 'edit'])->name('instructor.edit');
-
 Route::get('/estudiantes', [EstudianteController:: class, 'index'])->name('estudiantes.index');
 Route::post('/estudiantes', [EstudianteController::class, 'store'])->name('estudiantes.store');
 Route::get('/estudiantes/create', [EstudianteController::class, 'create'])->name('estudiantes.create');
@@ -37,3 +50,4 @@ Route::get('/inscripciones/create', [InscripcionController::class, 'create'])->n
 Route::delete('/inscripciones/{inscripcion}', [InscripcionController::class, 'destroy'])->name('inscripciones.destroy');
 Route::put('/inscripciones/{inscripcion}', [InscripcionController::class, 'update'])->name('inscripciones.update');
 Route::get('/inscripciones/{inscripcion}/edit', [InscripcionController::class , 'edit'])->name('inscripciones.edit');
+});
