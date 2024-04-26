385105a9355aedb3aaf1211fcbe165cca875c101
DayanaKCG <dcordoba3341@campuscecep.edu.co>
2024-04-25 21:28:40 -0500

    Carga de archivos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CursoController; 
 use Illuminate\Support\Facades\Route;
 
@@ -7,10 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/curso', [CursoController:: class, 'index'])->name('cursos.index');
-Route::post('/curso', [CursoController::class, 'store'])->name('cursos.store');
-Route::get('/curso/create', [CursoController::class, 'create'])->name('cursos.create');
-Route::delete('/curso/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
-Route::put('/curso/{curso}', [CursoController::class, 'update'])->name('cursos.update');
-Route::get('/curso/{curso}/edit', [CursoController::class , 'edit'])->name('cursos.edit');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+});
+
+require __DIR__.'/auth.php';
